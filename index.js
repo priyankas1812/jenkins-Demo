14af6b0d989fea39a0d9253059d81a70938b83c4
LAPTOP-6PH6D3FJ\PRIYANKA S <spriyankas2974@gmail.com>
2025-06-29 12:51:34 +0530

    added js code to count sum of 100 numbers

---
+++ index.js
@@ -0,0 +1,17 @@
+let sum = 0;
+let evenCount = 0;
+let oddCount = 0;
+
+for (let i = 1; i <= 100; i++) {
+  sum += i;
+
+  if (i % 2 === 0) {
+    evenCount++;
+  } else {
+    oddCount++;
+  }
+}
+
+console.log("Sum of first 100 numbers:", sum);
+console.log("Count of even numbers:", evenCount);
+console.log("Count of odd numbers:", oddCount);
