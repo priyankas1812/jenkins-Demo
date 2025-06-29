72d8e27b24e8501e2d78ba29df3ff4c237668e32
LAPTOP-6PH6D3FJ\PRIYANKA S <spriyankas2974@gmail.com>
2025-06-29 20:26:09 +0530

    removed a console log

--- index.js
+++ index.js
@@ -15,4 +15,3 @@ for (let i = 1; i <= 100; i++) {
 console.log("Sum of first 100 numbers:", sum);
 console.log("Count of even numbers:", evenCount);
 console.log("Count of odd numbers:", oddCount);
-console.log("the demo for jenkins project");
